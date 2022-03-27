0a59ecf61d25fce6a20718e36252a97c0fa7ae88
Caio <caiomarcelo367@gmail.com>
2022-03-27 18:06:00 -0400

    em andamento...

---
+++ URNA-ETAPAS.js
@@ -0,0 +1,50 @@
+let etapas=[
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:38111,
+                nome:'Naruto',
+                partido:'Konoha',
+                fotos:[
+                    {URL:'https://static.wikia.nocookie.net/geekpediabr/images/d/d1/B0452121-7F4B-490D-B523-6041C1DD317A.png/revision/latest?cb=20190722151129', legenda:'VEREADOR'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Goku',
+                partido: 'DBZ',
+                fotos: [
+                    {URL:'//sm.ign.com/t/ign_br/screenshot/default/goku_an6e.h720.jpg', legenda: 'VEREADOR'}
+                ]
+            },
+        ]
+    },
+    {
+    titulo: 'PREFEITO',
+    numeros:2,
+    candidatos: [
+        {
+            numero:99,
+            nome:'Vegeta',
+            partido: 'DBZ',
+            vice:'MajinBoo',
+            fotos:[
+                {URL: '//i.pinimg.com/736x/e4/ff/6e/e4ff6e8755bfd3139326d24718d64497.jpg', legenda:'PREFEITO'},
+                {URL:'https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q95/wp-content/uploads/2021/10/legiao_0Ovd7XqbTIos.png.jpeg', legenda:'VICE-PREFEITO', small:true}
+            ]
+        },
+        {
+            numero:84,
+            nome:'Sasuke',
+            partido:'Konoha',
+            vice:'Kakashi',
+            fotos:[
+                {URL:'https://static.wikia.nocookie.net/geekpediabr/images/0/0d/Uchiha_Sasuke.jpg/revision/latest?cb=20200411230429', legenda:'PREFEITO'},
+                {URL:'https://static.wikia.nocookie.net/dublagem/images/b/b8/Kakashi_Hatake_%28Boruto%29.png/revision/latest?cb=20210617065033&path-prefix=pt-br', legenda:'VICE-PREFEITO', small:true}
+            ]
+        },
+    ]
+  }
+];
